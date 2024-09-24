f0923bae27ce5745651b1b2cca0a49269ee42b23
Kinjal Chowdhury <kinjalchowdhury20@gmail.com>
2024-09-25 02:12:11 +0530

    auth wrapper fixed

--- components/AuthWrapper.tsx
+++ components/AuthWrapper.tsx
@@ -1,23 +1,20 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import SessionContext from "@/context/SessionContext";
 import Navbar from "./Navbar";
-import { usePathname } from "next/navigation";
 
 const AuthWrapper = ({ children }: { children: any }) => {
-  const { data: session, status } = useSession();
-  const [oldStatus, setOldStatus] = useState("");
-  const router = useRouter();
-  const pathname = usePathname()
-  useEffect(() => {
-    if (oldStatus == "authenticated" && status == "unauthenticated") {
-      router.push("/");
-    }
-    setOldStatus(status);
-  }, [status]);
-
+  const { data: session, status } = useSession(); 
+  // const [oldStatus, setOldStatus] = useState("");
+  // const router = useRouter();
+  // const pathname = usePathname()
+  // useEffect(() => {
+  //   if (oldStatus == "authenticated" && status == "unauthenticated") {
+  //     router.push("/");
+  //   }
+  //   setOldStatus(status);
+  // }, [status]);
   return <>
   <SessionContext.Provider value={JSON.stringify(session)}>
     {session &&  <Navbar session={session}/>}
